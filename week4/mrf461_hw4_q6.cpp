c9847388a24e3b15930ec0ac13ea2421f2be13e2
mattfemia <mattfemia1@gmail.com>
2019-05-02 02:16:57 -0400

    hw4 commented out idea for q6

--- week4/mrf461_hw4_q6.cpp
+++ week4/mrf461_hw4_q6.cpp
@@ -6,4 +6,34 @@
 //  Copyright © 2019 Matthew Femia. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int numList;
+int evens = 0;
+int odds = 0;
+int number;
+
+int main()
+{
+    cout<<"Please input a positive integer: ";
+    cin>>number;
+    
+    //CONVERT NUMBER OF DIGISTS IN NUMBER TO INT
+    //    length = strlen(string(number))
+    
+    
+    // ITERATE THROUGH NUMBERS UP TO USER NUMBER
+    //    for (int x=0; x<=number; x++) {
+    
+    // ITERATE OVER DIGITS IN NUMBER
+    //        for (int y=1; y<=length; y++)
+    
+    //CHECK IF EACH DIGIT IS EVEN OR ODD
+    //
+    // INCREMENT THE 'EVENS' AND 'ODDS' COUNTERS
+    
+    // PRINT TO THE CONSOLE IF EVENS > ODDS
+}
