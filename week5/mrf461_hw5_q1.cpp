19a51b8849ad945db3434709434fb7f1e87b25b8
mattfemia <mattfemia1@gmail.com>
2019-05-08 00:00:17 -0400

    hw5 q1 completed

---
+++ week5/mrf461_hw5_q1.cpp
@@ -0,0 +1,37 @@
+//
+//  mrf461_hw5_q1.cpp
+//  practice1
+//
+//  Created by Matthew Femia on 5/7/19.
+//  Copyright © 2019 Matthew Femia. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int num = 0;
+
+int multTable(int number);
+
+int main() {
+    
+    // Error check input for positive value
+    while (num <= 0){
+        cout<<"Please enter a positive integer: "<<endl;
+        cin>>num;
+    }
+    
+    multTable(num);
+    return 0;
+}
+
+int multTable(int number) {
+    for (int x=1; x <= number; x++) {
+        
+        for (int y=1; y<=number; y++) {
+            cout<<x * y<<"     ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+}
