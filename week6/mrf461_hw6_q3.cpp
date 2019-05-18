3005057b99a9141c1f509ddff54fe81345dc1b6d
mattfemia <mattfemia1@gmail.com>
2019-05-17 23:44:10 -0400

    hw6 q3 completed

--- week6/mrf461_hw6_q3.cpp
+++ week6/mrf461_hw6_q3.cpp
@@ -5,4 +5,26 @@
 //  Copyright © 2019 Matthew Femia. All rights reserved.
 //
 
-#include <stdio.h>
+double eApprox(int n);
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout.precision(30);
+    for (int n = 1; n <= 15; n++) {
+        cout<<"n = "<<n<<'\t'<<eApprox(n)<<endl;
+    }
+    
+    return 0;
+}
+
+double eApprox(int n){
+    double total = 1;
+    
+    for (int i = (n-1); i > 0; i--){
+        total = (total / i) + 1;
+    }
+    return total;
+}
+
