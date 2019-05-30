ecbc6c5b3a86ae9ba0027d6281de345eaed56a02
mattfemia <mattfemia1@gmail.com>
2019-05-30 00:46:37 -0400

    Ending status for hw8 q3

--- week8/mrf461_hw8_q3.cpp
+++ week8/mrf461_hw8_q3.cpp
@@ -6,4 +6,59 @@
 //  Copyright © 2019 Matthew Femia. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void reverseArray(int arr[], int arrSize);
+void removeOdd(int arr[], int& arrSize);
+void splitParity(int arr[], int arrSize);
+
+int main() {
+    int arr1[10] = {9, 2, 14, 12, -3, 3};
+    
+    int arr2[10] = {21, 12, 6, 7, 14};
+    int arr2Size = 5;
+    
+    reverseArray(arr1, 6);
+    cout<<endl;
+    
+    removeOdd(arr2, arr2Size);
+    cout<<endl;
+    
+    return 0;
+}
+
+void reverseArray(int arr[], int arrSize) {
+    int temp;
+    int forward = 0;
+    
+    for (int backward = arrSize - 1; backward >= (arrSize / 2); backward--) {
+        temp = arr[forward];
+        arr[forward] = arr[backward];
+        arr[backward] = temp;
+        forward++;
+    }
+    
+    for (int iterate = 0; iterate < arrSize; iterate++) {
+        cout<<arr[iterate]<<" ";
+    }
+    cout<<endl;
+}
+
+void removeOdd(int arr[], int& arrSize) {
+    
+    for (int iterate=0; iterate < arrSize; iterate++) {
+        if (arr[iterate] % 2 != 0) {
+            arrSize--;
+            int newNums = iterate;
+            for (newNums; newNums < arrSize; newNums++) {
+                arr[iterate + 1] = arr[iterate];
+            }
+        }
+    }
+}
+
+void splitParity(int arr[], int arrSize) {
+    
+}
