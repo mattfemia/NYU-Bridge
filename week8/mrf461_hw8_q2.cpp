901cf1da38b87ccbd4707102a02bcc4fe36d40a6
mattfemia <mattfemia1@gmail.com>
2019-05-29 23:29:51 -0400

    hw8 q2 completed

--- week8/mrf461_hw8_q2.cpp
+++ week8/mrf461_hw8_q2.cpp
@@ -6,4 +6,43 @@
 //  Copyright © 2019 Matthew Femia. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPalindrome(string str);
+
+int main() {
+    
+    string word;
+    
+    cout<<"Please enter a word: ";
+    cin>>word;
+    
+    // Test if word is palindrome + output proper statement
+    bool palindromeTest = isPalindrome(word);
+    
+    if (palindromeTest == true) {
+        cout<<word<<" is a palindrome"<<endl;
+    }
+    else {
+        cout<<word<<" is not a palindrome"<<endl;
+    }
+    
+    return 0;
+}
+
+bool isPalindrome(string str) {
+    string reverse;
+    
+    // Iterate over chars of string in reverse and concatenate to 'reverse' word
+    for (int position = (str.length() - 1); position >= 0; position--) {
+        reverse += str[position];
+    }
+    if (reverse == str){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
