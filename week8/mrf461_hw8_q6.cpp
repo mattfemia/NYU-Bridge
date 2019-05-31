ed3886774e82ee3d98d04c6f63d51d05d97b12ad
mattfemia <mattfemia1@gmail.com>
2019-05-31 15:23:21 -0400

    hw8 q6 completed

--- week8/mrf461_hw8_q6.cpp
+++ week8/mrf461_hw8_q6.cpp
@@ -25,29 +25,33 @@ int main() {
 }
 string scanWords(string phrase) {
     string sequence;
-    int position = 0;
     string word = "";
+    int position = 0;
+    
     for (int wordPosition = position; wordPosition <= phrase.length(); wordPosition++) {
+        // End word if a space is reached + add word to new sequence
         if (int(phrase[wordPosition]) == 32) {
+            
+            // Add to final output 'sequence'
             sequence += (word + " ");
+            
+            // Reset word and increase position
             word = "";
             position = wordPosition + 1;
         }
+        // Word starts with a letter --> numbers remain untouched
+        else if ((int(phrase[position]) <= 48) || int((phrase[position]) >= 57)) {
+            word += phrase[wordPosition];
+        }
+        // Word is a number --> change digits to x
         else if ((int(phrase[wordPosition]) >= 48) && (int(phrase[wordPosition]) <= 57)) {
-            
-            
-            if (int(phrase[wordPosition - 1]) || int(phrase[wordPosition + 1]) == 32) {
-                word += "x";
-            }
-            
-            else {
-                word += phrase[wordPosition];
-            }
+            word += "x";
         }
         else {
             word += phrase[wordPosition];
         }
     }
+    // Add last word to sequence
     sequence += word;
     return sequence;
 }
