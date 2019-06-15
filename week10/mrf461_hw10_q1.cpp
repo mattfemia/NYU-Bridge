2a32754bc287d7e67915b965ee4722229f4d1efd
mattfemia <mattfemia1@gmail.com>
2019-06-15 01:55:22 -0400

    hq10 q1 completed. q3 half. q2 none

--- week10/mrf461_hw10_q1.cpp
+++ week10/mrf461_hw10_q1.cpp
@@ -6,4 +6,64 @@
 //  Copyright © 2019 Matthew Femia. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+string* createWordsArray(string sentence, int& outWordsArrSize);
+
+int main(){
+    
+    string sentence;
+    int numberOfWords;
+    
+    cout << "Please enter a sentence: " << endl;
+    getline(cin, sentence);
+    
+    string *wordArray = createWordsArray(sentence, numberOfWords);
+    cout << "There are " << numberOfWords << " words in the input:" << endl;
+    for (int x = 0; x < numberOfWords; x++){
+        cout << "Word #" << x + 1 << " is " << wordArray[x] << endl;
+    }
+    return 0;
+}
+
+
+string* createWordsArray(string sentence, int& outWordsArrSize){
+    vector<int> space_vector;
+    int position = 0;
+    
+    // Create vector 'space_vector' with positions of spaces in the sentence
+    while (sentence.find(" ", position) != string::npos){
+        double space = sentence.find(" ", position);
+        space_vector.push_back(space);
+        position = space + 1;
+    }
+    
+    int *array_position = &outWordsArrSize;
+    int *wordArraySize = array_position;
+    *wordArraySize = int(space_vector.size());
+    
+    const int SIZE = *wordArraySize;
+    string *word;
+    word = new string[SIZE];
+    
+    
+    // Fill array based space_vector
+    position = 0;
+    for (int x = 0; x < SIZE; x++){
+        word[x] = sentence.substr(position, (space_vector[x] - position));
+        position = space_vector[x] + 1;
+        
+    }
+    // Account for there being no space at the end of the input to get the final word
+    double remaining = (sentence.length() - position);
+    if (remaining > 0){
+        word[SIZE] = sentence.substr(position, remaining);
+        *wordArraySize = SIZE + 1;
+    }
+    
+    return word;
+}
--- week10/mrf461_hw10_q3.cpp
+++ week10/mrf461_hw10_q3.cpp
@@ -6,4 +6,54 @@
 //  Copyright © 2019 Matthew Femia. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void withoutVector();
+void withVector();
+
+int main(){
+    
+    withVector();
+    
+    return 0;
+}
+
+void withoutVector(){
+    
+}
+
+void withVector(){
+    vector<int> numList;
+    int number;
+    
+    cout << "Please enter a sequence of positive integers, each in a separate line." << endl;
+    cout << "End your input by typing -1." << endl;
+    cin >> number;
+    
+    while (number != -1){
+        numList.push_back(number);
+        cin >> number;
+    }
+    
+    int search;
+    cout << "Please enter the number you want to search." << endl;
+    cin >> search;
+    
+    const int SIZE = int(numList.size());
+    int positionsArray[SIZE];
+    int numOfPositions = 0;
+    for (int x = 0; x <= SIZE; x++){
+        if (numList[x] == search){
+            positionsArray[numOfPositions] = x + 1;
+            numOfPositions++;
+        }
+    }
+    cout << search << " shows in lines ";
+    for (int i = 0; i < numOfPositions; i++){
+        cout << positionsArray[i] << " ";
+    }
+    cout << endl;
+}
