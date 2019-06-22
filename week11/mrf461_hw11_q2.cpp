e1b50922d3feed1a76753b9a53110ac2351ae038
mattfemia <mattfemia1@gmail.com>
2019-06-21 23:46:48 -0400

    Submit completed hw11 q2

--- week11/mrf461_hw11_q2.cpp
+++ week11/mrf461_hw11_q2.cpp
@@ -6,4 +6,41 @@
 //  Copyright © 2019 Matthew Femia. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// a.)
+int sumOfSquares(int arr[], int arrSize){
+    if (arrSize > 0){
+        return sumOfSquares(arr, (arrSize - 1)) + (pow(double(arr[arrSize - 1]), 2.0));
+    }
+    else {
+        return 0;
+    }
+}
+
+// b.)
+bool isSorted(int arr[], int arrSize){
+    if (arrSize > 0){
+        bool value = isSorted(arr, (arrSize - 1));
+        if (arr[arrSize] > value){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else if (arrSize == 0){
+        bool following = arr[arrSize + 1];
+        if (arr[arrSize] > following){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return true;
+    }
+}
