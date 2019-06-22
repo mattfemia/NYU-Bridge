391a191099514a5013cf2c1d330deffecb5ab7d5
mattfemia <mattfemia1@gmail.com>
2019-06-21 23:56:29 -0400

    Completed hw11 q3.1

--- week11/mrf461_hw11_q3.cpp
+++ week11/mrf461_hw11_q3.cpp
@@ -6,4 +6,47 @@
 //  Copyright © 2019 Matthew Femia. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int minInArray1(int arr[], int arrSize);
+
+int main() {
+    
+    int arr[10] = {9, -2, 14, 12, 3, 6, 2, 1, -9, 15};
+    int res1, res2, res3, res4;
+    res1 = minInArray1(arr, 9);
+    //    res2 = minInArray2(arr, 0, 9);
+    cout<<res1<<endl; //should both be -9
+    //    res3 = minInArray2(arr, 2, 5);
+    
+    return 0;
+    
+    return 0;
+}
+
+int minInArray1(int arr[], int arrSize){
+    int lowest = 0;
+    if (arrSize > 0){
+        int value = minInArray1(arr, (arrSize - 1));
+        if (arr[arrSize] > value){
+            lowest = value;
+        }
+        else {
+            lowest = arr[arrSize];
+        }
+    }
+    else if (arrSize == 0){
+        int intAfterZero = arr[arrSize + 1];
+        if (arr[arrSize] > intAfterZero){
+            lowest = intAfterZero;
+        }
+        else {
+            lowest = arr[arrSize];
+        }
+    }
+    else {
+        return 404;
+    }
+    return lowest;
+}
