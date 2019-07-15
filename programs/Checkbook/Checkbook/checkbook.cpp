b9ca7e61c1311747d031aac9aa597cf74382fe38
mattfemia <mattfemia1@gmail.com>
2019-07-14 23:53:07 -0400

    Add constified get functions (checkbook.cpp

---
+++ programs/Checkbook/Checkbook/checkbook.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  checkbook
+//
+//  Created by Matthew Femia on 7/14/19.
+//  Copyright © 2019 Matthew Femia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
