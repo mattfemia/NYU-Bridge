8248aca1f4d24b666990915d44decab07aeb6ec4
mattfemia <mattfemia1@gmail.com>
2019-07-21 20:10:43 -0400

    Create skeleton for week15 file processing assignment

--- week14/mrf461_hw14.cpp
+++ week14/mrf461_hw14.cpp
@@ -7,28 +7,46 @@
 //  Copyright © 2019 Matthew Femia. All rights reserved.
 //
 
+// TODO: USE RECURSION
+
 #include <iostream>
 using namespace std;
 
 void divideConquer(int array[], int sizeOfArray);
 
+class SubArray{
+    int maximum;
+    int minimum;
+}
+
 int main() {
     
-    // TODO: Take user input into a dynamic array
     const int SIZE = 3;
-    int numArray[SIZE] = {1, 2, 3};
+    int numArray[SIZE] = {1, 3, 2};
     
+    SubArray arrayValues;
     int minVal;
     int maxVal;
     
-    // TODO: Determine sizeOfArray and conditionals for each SIZE
     if (SIZE == 1){
-        minVal = numArray[0];
-        maxVal = numArray[0];
-        cout << "The minimum value = " << minVal << endl;
-        cout << "The maximum value = " << maxVal << endl;
+        arrayValues.minimum = numArray[0];
+        arrayValues.maximum = numArray[0];
+        cout << "The minimum value = " << arrayValues.minimum << endl;
+        cout << "The maximum value = " << arrayValues.maximum << endl;
     }
     else if (SIZE == 2){
+        if (numArray[0] < numArray[1]){
+            arrayValues.minimum = numArray[0];
+            arrayValues.maximum = numArray[1];
+        }
+        else{
+            arrayValues.minimum = numArray[1];
+            arrayValues.maximum = numArray[0];
+        }
+        cout << "The minimum value = " << arrayValues.minimum << endl;
+        cout << "The maximum value = " << arrayValues.maximum << endl;
+    }
+    else if (SIZE == 3){
         if (numArray[0] < numArray[1]){
             minVal = numArray[0];
             maxVal = numArray[1];
@@ -37,11 +55,13 @@ int main() {
             minVal = numArray[1];
             maxVal = numArray[0];
         }
-        cout << "The minimum value = " << minVal << endl;
-        cout << "The maximum value = " << maxVal << endl;
-    }
-    else if (SIZE == 3){
         
+        if (numArray[2] < minVal){
+            minVal = numArray[2];
+        }
+        if (numArray[2] > maxVal){
+            maxVal = numArray[2];
+        }
     }
     else{
         divideConquer(numArray, SIZE);
@@ -57,6 +77,7 @@ void divideConquer(int array[], int sizeOfArray){
     else{
         mid = (sizeOfArray + 1) / 2;
     }
+    
     // Lower half of array
     int lowMin, lowMax;
     if (array[0] < array[1]){
@@ -77,7 +98,7 @@ void divideConquer(int array[], int sizeOfArray){
         }
     }
     
-    // Higher half of array
+    // Greater half of array
     int highMin, highMax;
     if (array[mid] < array[mid + 1]){
         highMin = array[mid];
@@ -117,3 +138,4 @@ void divideConquer(int array[], int sizeOfArray){
     cout << "The minimum value = " << minimum << endl;
     cout << "The maximum value = " << maximum << endl;
 }
+
---
+++ week15/mrf461_week15.cpp
@@ -0,0 +1,48 @@
+//
+//  mrf461_week15.cpp
+//  week15
+//
+//  Created by Matthew Femia on 7/21/19.
+//  Copyright © 2019 Matthew Femia. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class EmployeeData{
+private:
+    string employeeName;
+    double employeeNumber;
+    double payRate;
+public:
+    // string getName() const return { string employeeName };
+    // double getNumber() const return { double employeeNumber };
+    // double getRate() const return { double payRate };
+    
+    string setEmployeeName();
+    double setEmployeeNumber();
+    double setPayRate();
+};
+
+int main(){
+    
+    return 0;
+}
+
+
+
+string EmployeeData::setEmployeeName(){
+    
+    
+}
+
+double EmployeeData::setEmployeeNumber(){
+    
+}
+
+double EmployeeData::setPayRate(){
+    
+}
+
+
